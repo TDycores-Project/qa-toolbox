9966dc243f1883a5b38d8b2f383e404d9b042ae6
Glenn Hammond <gehammo@sandia.gov>
2019-11-20 16:28:40 -0800

    Added exit status

--- .travis/test-qa_toolbox.sh
+++ .travis/test-qa_toolbox.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
 python3 qa_test_suite.py
+
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "QA Toolbox tests failed" >&2
+  exit 1
+else
+  echo "QA Toolbox tests succeeded" >&2
+  exit 0
+fi
+
+
