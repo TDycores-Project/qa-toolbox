d616ae7b9e515733729ec56b2c420c9160739d8b
julia.camargo <julia.camargo@pnnl.gov>
2021-04-30 12:10:21 -0700

    added solution to QASolutionWriter

--- simulator_modules/stomp.py
+++ simulator_modules/stomp.py
@@ -13,21 +13,10 @@ from simulator_modules.simulator import QASimulator
 eps = 1e-6
 successful_exit_code = 86
 
-# update these names??
-h5_mapping = {}
-h5_mapping['liquid_pressure'] = 'Liquid Pressure'
-h5_mapping['liquid_saturation'] = 'Liquid Saturation'
-h5_mapping['Total_Tracer [M]'] = 'Tracer'
-h5_mapping['Material_ID'] = 'Material_ID'
-h5_mapping['Liquid_Pressure [Pa]'] = 'Liquid Pressure'
-h5_mapping['Liquid_Saturation'] = 'Liquid Saturation'
-h5_mapping['Liquid_Pressure'] = 'Liquid Pressure'
-
-obs_mapping = {}
-obs_mapping['Liquid_Pressure'] = 'Liquid Pressure'
-obs_mapping['Liquid_Saturation'] = 'Liquid Saturation'
-obs_mapping['Liquid Pressure [Pa]'] = 'Liquid Pressure'
-
+time_mapping = {}
+time_mapping['Aqueous Saturation'] = 'Liquid Saturation'
+time_mapping['Aqueous Pressure, pa'] = 'Liquid Pressure'
+n_variables = len(time_mapping)
 
 class QASimulatorSTOMP(QASimulator):
 
@@ -57,19 +46,67 @@ class QASimulatorSTOMP(QASimulator):
 
     def convert_solution_to_common_h5(self, filename):
         debug_push('QASimulatorSTOMP convert_solution_to_common_h5')
-        root = filename.rsplit('.', 1)[0]
-        solution_filename = '{}_stomp.h5'.format(root)
+        # root = filename.rsplit('.', 1)[0]
+        solution_filename = '{}_stomp.h5'.format(filename)
         solution = QASolutionWriter(solution_filename)
+        root = os.path.dirname(filename)
 
         tslice_out = []
-        for root, dire, files in os.walk('.'):
+        x = []
+        y = []
+        z = []
+        first_file = True
+
+        for r, dirct, files in os.walk(root):
             for name in files:
-                if (name.startswith('plot'):
-                    tslice_out.append(name)
+                if name.startswith('plot') and not name.endswith('.dat'):
+                    tslice_out.append(root + '/' + name)
 
         for i in range(len(tslice_out)):
+            if first_file:
+                fin = open(tslice_out[i], 'r')
+                for line in fin:
+                    line = line.strip()
+                    if line == []:
+                        continue
+                    #get coordinates
+                    if ('X-Direction Nodal Vertices, m' in line):
+                        for line in fin:
+                            line = line.strip()
+                            words = line.split()
+                            if not words:
+                                break
+                            # get x-centroid
+                            x_centroid = (float(words[0]) + float(words[1])) * 0.5
+                            x.append(x_centroid)
+
+                    if ('Y-Direction Nodal Vertices, m' in line):
+                        for line in fin:
+                            line = line.strip()
+                            words = line.split()
+                            if not words:
+                                break
+                            # get y-centroid
+                            y_centroid = (float(words[0]) + float(words[2])) * 0.5
+                            y.append(y_centroid)
+
+                    if ('Z-Direction Nodal Vertices, m' in line):
+                        for line in fin:
+                            line = line.strip()
+                            words = line.split()
+                            if not words:
+                                break
+                            # get z-centroid
+                            z_centroid = (float(words[0]) + float(words[4])) * 0.5
+                            z.append(z_centroid)
+
+                fin.close()
+                solution.write_coordinates(x,y,z)
+                first_file = False
             # read file
             fin = open(tslice_out[i], 'r')
+            time = None
+            all_values = []
             for line in fin:
                 line = line.strip()
                 if line == []:
@@ -78,70 +115,20 @@ class QASimulatorSTOMP(QASimulator):
                 if ('Time =  ' in line):
                     words = line.split()
                     time_units = words[5].split(',')
-                    time = time_units[0]
-                    units = time_units[1]
-                #get coordinates
-                if ('X-Direction Nodal Vertices, m' in line):
-                    # continue reading the following lines
-                    for line in fin:
-                        line = line.strip()
-                        words = line.split()
-                        # get x-centroid
-                        x_centroid = (float(words[0]) + float(words[1])) * 0.5
-                        x.append(x_centroid)
-                        if not words:
-                            break
-                if ('Y-Direction Nodal Vertices, m' in line):
-                    # continue reading the following lines
-                    for line in fin:
-                        line = line.strip()
-                        words = line.split()
-                        line_counter += 1
-                        # get y-centroid
-                        y_centroid = (float(words[0]) + float(words[2])) * 0.5
-                        y.append(y_centroid)
-                        if not words:
-                            break
-                if ('Z-Direction Nodal Vertices, m' in line):
-                    # continue reading the following lines
-                    for line in fin:
-                        line = line.strip()
-                        words = line.split()
-                        # get z-centroid
-                        z_centroid = (float(words[0]) + float(words[4])) * 0.5
-                        z.append(z_centroid)
-                        if not words:
-                            break
-                if ('Aqueous Saturation' in line):
-                    # continue reading the following lines
-                    for line in fin:
-                        line = line.strip()
-                        words = line.split()
-                        # get saturation
-                        for sat in words:
-                            saturation.append(float(sat))
-                        if not words:
-                            break
-                if ('Aqueous Pressure, pa' in line):
-                    for line in fin:
-                        line = line.strip()
-                        words = line.split()
-                        # get pressure
-                        for pres in words:
-                            pressure.append(float(pres))
-                        if not words:
-                            break
+                    time = float(time_units[0])
+                    t_units = time_units[1]
+                    solution.set_time_unit(t_units)
+
+                for j, (key, v_name) in enumerate(time_mapping.items()):
+                    if (key in line):
+                        for line in fin:
+                            line = line.strip()
+                            words = line.split()
+                            if not words:
+                                break
+                            for var_values in words:
+                                all_values.append(float(var_values))
+                        all_values_np = np.asarray(all_values, dtype=np.float64).transpose()
+                        solution.write_dataset(time,all_values_np, key,'Time Slice')
             fin.close()
 
-    def update_dict(self, output_options):
-        debug_push('QASimulatorSTOMP update_dict')
-#        for value in output_options.values():
-#            new_entry=[x.strip() for x in value.split(',')]
-#            h5_mapping[new_entry[0]]=new_entry[1]
-#            obs_mapping[new_entry[0]]=new_entry[1]
-        for key, value in output_options.items():
-            #            new_entry=value.strip()
-            h5_mapping[key.strip()]=value.strip()
-            obs_mapping[key.strip()]=value.strip()
-
-        debug_pop()
