1f5b72b7fd825335d64495cc09a399019c4afbf9
Calvin Madsen <madsenc004@gmail.com>
2023-07-19 09:00:32 -0600

    phreeqc code midway complete

--- simulator_modules/phreeqc.py
+++ simulator_modules/phreeqc.py
@@ -15,7 +15,7 @@ class QASimulatorPHREEQC(QASimulator):
         debug_push('QASimulatorPHREEQC init')
         super(QASimulatorPHREEQC,self).__init__(path)
         self._name = 'phreeqc'
-        self._suffix = '.pqi'
+        self._suffix = '.in'
         debug_pop()
         
     def run(self,filename,annotation,np):
@@ -25,7 +25,8 @@ class QASimulatorPHREEQC(QASimulator):
         command = []
         command.append(self._get_full_executable_path())
         
-        output_filename = filename.split('.')[0] + '.pqo'
+        root = filename.split('.')[0]
+        output_filename = root + '.out'
         
         command.append(filename)
         command.append(output_filename)
@@ -35,121 +36,101 @@ class QASimulatorPHREEQC(QASimulator):
         debug_pop()
         
         # convert the output file to the common h5 version
-        solution_filename = self.convert_solution_to_common_h5(filename, output_filename)
+        solution_filename = self.convert_solution_to_common_h5(filename)
         debug_pop()
         
         return solution_filename
     
-    def convert_solution_to_common_h5(self,input_file_name, output_filename):
+    def convert_solution_to_common_h5(self,input_filename):
         debug_push('QASimulatorPHREEQC convert_solution_to_common_h5')
-        solution_filename = get_h5_output_filename(input_file_name, self._name)
+        solution_filename = get_h5_output_filename(input_filename, self._name)
+        selected_output = input_filename.split(self._name)[0].rstrip('_') + '.sel'
+
         solution = QASolutionWriter(solution_filename)
-        """
-        measurements = []
+        # Ca+2 HCO3- H+
+        # ['Calcite_Rate [mol_m^3_sec]', 'Calcite_VF [m^3 mnrl_m^3 bulk]', 'Free_Ca++ [M]', 'Free_H+ [M]', 'Free_HCO3- [M]', 'Material_ID', 'pH']
+        # 1DCalcite-x&t.sel
+        measurement_labels = []
         measurement_units = []
         data_dict = {}
-        h5_dataset_name_mapping = {'Temp': 'Temperature [C]', 
-                                   'Depth': 'Depth [m]',
-                                   'Head': 'Liquid Pressure',
-                                   'Moisture' : 'Liquid Saturation'}
+        h5_dataset_name_mapping = {'m_Ca+2': 'Free_Ca++ [m]',
+                                   'm_H+': 'Free_H+ [m]',
+                                   'm_HCO3-' : 'Free_HCO3- [m]',
+                                   'Ca+2' : 'Total_Ca++ [m]'}
         group_name = 'Time Slice'  
         units_first = 1
         labels_first = 1
         coords_first = 1
-        moisture_first = 1
-        
-        liquid_sat_denom = 1
+
         time = 0
-        """
+        time_index = 0
+        dist_x_index = 0
+        
         try:
-            with open(output_filename, 'r') as f:
-                print("FOUND")
-                """
+            with open(selected_output, 'r') as f:
+                
                 for line in f:
                     # Split each line into a list separated by whitespace
                     line = line.strip().split()
                     
-                    # Find time units
-                    if units_first and 'Units:' in line:
-                        if 'seconds' in line:
-                            solution.set_time_unit('s')
-                        elif 'days' in line:
-                            solution.set_time_unit('d')
-                        elif 'months' in line:
-                            solution.set_time_unit('m')
-                        elif 'years' in line:
-                            solution.set_time_unit('y')
-                        units_first = 0
+                    # Find measurement labels and time index (always on first line)
+                    if labels_first:
+                        for i, label in enumerate(line):
+                            measurement_labels.append(label)
+                            if label == 'step':
+                                time_index = i
+                            if label == 'dist_x':
+                                dist_x_index = i
                         
-                    # Find time
-                    elif len(line) == 2 and 'Time:' == line[0]:
-                        time = float(line[1])
+                        labels_first = 0
+                        continue
                     
-                    # Get list of measurements
-                    elif labels_first and len(line) > 1 and line[0] == 'Node':
-                        for label in line:
-                            measurements.append(label)
+                    # Set time units
+                    solution.set_time_unit('y')
+                        
+                    # Find current time (step)
+                    time = float(line[time_index])
                             
                     # Get list of measurement units
-                    elif labels_first and len(line) == 10:
-                        for label in line:
-                            measurement_units.append(label)
-                        labels_first = 0
+                    #elif labels_first and len(line) == 10:
+                    #    for label in line:
+                    #        measurement_units.append(label)
+                    #    labels_first = 0
                         
                     # Append data points to corresponding measurement key
-                    elif len(line) == 11 and line[0] != 'Node':
-                        for i, value in enumerate(line):
-                            if i == 0:
-                                continue
-                            value = float(value)
-                            
-                            # convert from cm to m
-                            if measurement_units[i-1] in ['[L]','[L/T]']: #cm
-                                value *= 0.01
-                            elif measurement_units[i-1] == '[1/L]': # 1/cm
-                                value *= 100.
-                            
-                            # convert moisture to liquid saturation
-                            if moisture_first and measurements[i] == 'Moisture':
-                                liquid_sat_denom = value
-                                moisture_first = 0    
-                            if  measurements[i] == 'Moisture':
-                                value /= liquid_sat_denom
-                                
-                            # convert head to liquid pressure:
-                            if  measurements[i] == 'Head':
-                                value = (value * 9780) + 101325
-                            
-                            # append data values to corresponding measurement key
-                            if measurements[i] not in data_dict.keys():
-                                data_dict[measurements[i]] = [value]
-                            else:
-                                data_dict[measurements[i]].append(value)
+                    for i, value in enumerate(line):
+                        value = float(value)
+                        if measurement_labels[i] not in data_dict:
+                            data_dict[measurement_labels[i]] = [value]
+                        else:
+                            data_dict[measurement_labels[i]].append(value)
                     
                     # Write each dataset to solution for corresponding measurement
-                    elif 'end' in line:
+                    if line[dist_x_index] == '100':
                         for key, values in data_dict.items():
-                            if key in h5_dataset_name_mapping.keys():
+                            if key in h5_dataset_name_mapping:
                                 dataset_name = h5_dataset_name_mapping[key]
                             else:
                                 dataset_name = key
                                 
                             data_dict[key] = np.array([[values]])
+                            data_dict[key] = np.reshape(data_dict[key], (102, 1, 1))
+                            
                             solution.write_dataset(time, data_dict[key], dataset_name, group_name)
                             
                             # Write the coords
                             if coords_first:
                                 coords_first = 0
                                 # for 1D tests
-                                x = 0.5 * (np.arange(1) + 1)
-                                y = 0.5 * (np.arange(1) + 1)
-                                z = np.reshape(data_dict['Depth'], -1)
+                                x = np.reshape(data_dict['dist_x'], -1)
+                                y = 0.05 * (np.arange(1) + 1)
+                                z = 0.05 * (np.arange(1) + 1)
                                 solution.write_coordinates(x, y, z)
                             
                         data_dict = {}  # reset the dict for next time value
-                """
+                    
         except FileNotFoundError:
-            print('output file not found')
+            print(f'selected output file {selected_output} not found')
                 
         debug_pop()
         return solution_filename
